b4fca97996f972c64e8be0dbf4720fc2035bf538
ysenel <yasin.senel@matchable.io>
2015-12-29 22:14:53 +0100

    create, getAll, getOne, deleteAll and deleteOne added on article modul

---
+++ server/article.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+
+
+
+var articleSchema = new mongoose.Schema({
+	title : String
+});
+
+var articleModel = mongoose.model('articles', articleSchema);
+
+
+exports.create = function (req, res) {
+	if (typeof req.body.title != 'undefined'){
+		console.log("nnnnnnn");
+		var article = new articleModel({title : req.body.title});
+		article.save(function (err, doc) {
+	  		if (err) { throw err; }
+	  		console.log('article ajouté avec succès !');
+	  		res.json(doc);
+		});
+	}
+	else
+		res.json("error");
+};
+
+exports.getAll = function (res, res) {
+	var query = articleModel.find(null);
+	query.exec(function (err, articles) {
+  		if (err) { throw err; }
+  		return res.json(articles);
+	});
+};
+
+exports.getOne = function (req, res) {
+	var query = articleModel.findById({_id : req.params.id});
+	query.exec(function (err, article) {
+  		if (err) { throw err; }
+  		return res.json(article);
+	});
+};
+
+exports.deleteOne = function (req, res) {
+	var id = req.params.id;
+	articleModel.remove({_id : id}, function (err, result) {
+  	if (err) { throw err; }
+  	console.log('Article id = ' + id + ' supprimé.');
+  	return res.json(result);
+	});
+};
+
+exports.deleteAll = function (req, res) {
+	articleModel.remove(null, function (err, result) {
+  	if (err) { throw err; }
+  	console.log('Tout les articles ont été supprimés.');
+  	return res.json(result);
+	});
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var article = require("./article");
 
 
 var app = express();
@@ -11,31 +12,12 @@ var db = mongoose.connect('mongodb://localhost/MyWordPress', function(err) {
   if (err) { throw err; }
 });
 
-var ArticleSchema = new mongoose.Schema({title : String});
 
-var ArticleModel = mongoose.model('articles', ArticleSchema);
-
-
-app.get('/articles', function(req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	var query = ArticleModel.find(null);
-	query.exec(function (err, articles) {
-  		if (err) { throw err; }
-  		return res.json(articles);
-});
-
-});
-
-app.post('/article', function(req, res) {
-
-	console.log(req.body.title);
-	var article = new ArticleModel({title : req.body.title});
-	console.log("titititi");
-	article.save(function (err) {
-	  if (err) { throw err; }
-	  console.log('article ajouté avec succès !');
-	});
-});
+app.get('/app/articles', article.getAll);
+app.post('/app/article', article.create);
+app.get('/app/article/:id', article.getOne);
+app.delete('/app/article/:id', article.deleteOne);
+app.delete('/app/articles', article.deleteAll);
 
 
 app.listen(8080);
