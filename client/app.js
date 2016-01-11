8061a58327e4facf17269c7d74609f0d2a28b8c6
ysenel <yasin.senel@matchable.io>
2016-01-11 02:07:31 +0100

    articles finished, can get, add, remove articles but not edit it

--- client/app.js
+++ client/app.js
@@ -1,18 +1,32 @@
-var app = angular.module('MyWordPress', ['ui.router']);
+var app = angular.module('MyWordPress', ['ui.router', 'ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo : "/"});
+});
 
 app.config(['$stateProvider',
 	function($stateProvider) {
-		$stateProvider.state("header", {
+		$stateProvider
+        .state("home", {
+                url : "/",
 				views : {
 					"header" : {templateUrl: "template/header.html"},
-					"article" : {templateUrl: "template/article.html"}
+					"content" : {templateUrl: "template/article.html"}
 				}
 			}
-		);
+		)
+        .state("addArticle", {
+                url : "/addArticle",
+                views : {
+                    "header" : {templateUrl: "template/header.html"},
+                    "content" : {templateUrl: "template/newArticle.html"}
+                }
+            }
+        );
 }])
 
 .run(['$state', function ($state) {
-   $state.transitionTo('header');
+   $state.transitionTo('home');
 }]);
 
 app.controller("articleCtrl", function ($scope, $http) {
@@ -26,6 +40,21 @@ app.controller("articleCtrl", function ($scope, $http) {
     	$http.delete("http://localhost:8080/app/article/" + id)
     	.then(function(res) {
     		console.log(res);
+            $("#" + id).remove();
+
+    });
+    }
+});
+
+app.controller("newArticle", function ($scope, $http, $state) {
+    $scope.newArticle = {};
+    $scope.newArticle.date = new Date();
+    $scope.addNewArticle = function () {
+        console.log($scope.newArticle.title)
+        $http.post("http://localhost:8080/app/article", $scope.newArticle)
+        .then(function(res) {
+            console.log("envoyé");
+            $state.go('home');
 
     });
     }
